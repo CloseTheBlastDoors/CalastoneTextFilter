a7115066714d25a29e742d42c5d501a91204e6af
Richard Hart <richard.hart@finova.tech>
2026-06-28 09:08:16 +0100

    MiddleVowel filter tests and first pass implementation

---
+++ CalastoneTextFilter.App/Filters/MIddleVowelFilter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalastoneTextFilter.App.Filters
+{
+    public class MiddleVowelFilter : IWordFilter
+    {
+        public bool ShouldKeep(ReadOnlySpan<char> word)
+        {
+            if (word.IsEmpty) return true;
+
+            string wordAsString = word.ToString();
+            int mid = wordAsString.Length / 2;
+
+            if (wordAsString.Length % 2 == 1) // Odd length
+            {
+                return !IsVowel(wordAsString[mid]);
+            }
+            else // Even length
+            {
+                return !IsVowel(wordAsString[mid - 1]) && !IsVowel(wordAsString[mid]);
+            }
+        }
+
+        private bool IsVowel(char c)
+        {
+            return "aeiou".Contains(c, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
