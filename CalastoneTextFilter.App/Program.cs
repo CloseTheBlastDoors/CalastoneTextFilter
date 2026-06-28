04c3a563fcddac4dfa9831d12f914d7cfad1ead8
Richard Hart <richard.hart@finova.tech>
2026-06-28 09:49:18 +0100

    Set up console app to use first command line argument as path to input text file. Added Input.txt that contains the text from the assignment and set up the project to process it by default when run in Debug

--- CalastoneTextFilter.App/Program.cs
+++ CalastoneTextFilter.App/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalastoneTextFilter.App;
+using CalastoneTextFilter.App.Filters;
+
+if (args.Length == 0)
+{
+    Console.Error.WriteLine("Usage: TextFilter.App <path-to-file.txt>");
+    return 1;
+}
+
+string path = args[0];
+
+if (!File.Exists(path))
+{
+    Console.Error.WriteLine($"File not found: {path}");
+    return 1;
+}
+
+string text = File.ReadAllText(path);
+
+const int MIN_LENGTH = 3;
+const char CHARACTER_TO_FILTER = 't';
+
+var filterPipeline = new FilterPipeline(
+[
+    new MiddleVowelFilter(),
+    new MinLengthFilter(MIN_LENGTH),
+    new ContainsCharacterFilter(CHARACTER_TO_FILTER)
+]);
+
+string result = filterPipeline.Apply(text);
+
+Console.WriteLine(result);
+return 0;
