b7a122d5b3b0afe6da7ffd66de2e9bce26c4c991
Richard Hart <richard.hart@finova.tech>
2026-06-28 10:59:07 +0100

    Singleton for the pipeline is fine, but really the filters should be transient

--- CalastoneTextFilter.App/FilterPipeline/ServiceCollectionExtensions.cs
+++ CalastoneTextFilter.App/FilterPipeline/ServiceCollectionExtensions.cs
@@ -13,16 +13,16 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddMinLengthFilter(this IServiceCollection services, int length)
     {
-        return services.AddSingleton<IWordFilter, MinLengthFilter>(provider => new MinLengthFilter(length));
+        return services.AddTransient<IWordFilter, MinLengthFilter>(provider => new MinLengthFilter(length));
     }
 
     public static IServiceCollection AddCharacterFilter(this IServiceCollection services, char character)
     {
-        return services.AddSingleton<IWordFilter, ContainsCharacterFilter>(provider => new ContainsCharacterFilter(character));
+        return services.AddTransient<IWordFilter, ContainsCharacterFilter>(provider => new ContainsCharacterFilter(character));
     }
 
     public static IServiceCollection AddMiddleVowelFilter(this IServiceCollection services)
     {
-        return services.AddSingleton<IWordFilter, MiddleVowelFilter>();
+        return services.AddTransient<IWordFilter, MiddleVowelFilter>();
     }
 }
