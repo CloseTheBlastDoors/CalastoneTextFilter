1e4aba73de461f77e1956abdfd4330729c8682a6
Richard Hart <richard.hart@finova.tech>
2026-06-28 08:34:03 +0100

    Add MinLengthFilter tests and initial implementation to pass tests

---
+++ CalastoneTextFilter.App/Filters/MinLengthFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalastoneTextFilter.App.Filters
+{
+    public class MinLengthFilter
+    {
+        public bool ShouldKeep(string word)
+        {
+            return word.Length >= 3;
+        }
+    }
+}
---
+++ CalastoneTextFilter.Tests/MinLengthFilterTests.cs
@@ -0,0 +1,34 @@
+﻿using CalastoneTextFilter.App.Filters;
+
+namespace CalastoneTextFilter.Tests
+{
+    public class MinLengthFilterTests
+    {
+        private readonly MinLengthFilter _filter = new();
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("to")]
+        public void ShouldKeep_ReturnsFalse_WhenLengthLessThan3(string word)
+        {
+            Assert.False(_filter.ShouldKeep(word));
+        }
+
+        [Theory]
+        [InlineData("they")]
+        [InlineData("word")]
+        [InlineData("hello")]
+        [InlineData("beginning")]
+        public void ShouldKeep_ReturnsTrue_WhenLengthAtLeast3(string word)
+        {
+            Assert.True(_filter.ShouldKeep(word));
+        }
+
+        [Fact]
+        public void ShouldKeep_ExactlyLength3_ReturnsTrue()
+        {
+            Assert.True(_filter.ShouldKeep("abc"));
+        }
+    }
+}
