3d163b421cfa194a7f7db1595aa4c9157e088d9a
Richard Hart <richard.hart@finova.tech>
2026-06-28 10:28:46 +0100

    Split ServiceCollectionExtensions into own file

--- CalastoneTextFilter.App/FilterPipeline/FilterPipeline.cs
+++ CalastoneTextFilter.App/FilterPipeline/FilterPipeline.cs
@@ -1,32 +1,7 @@
 ﻿using CalastoneTextFilter.App.FilterPipeline.Filters;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CalastoneTextFilter.App.FilterPipeline;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddFilterPipeline(this IServiceCollection services)
-    {
-        services.AddSingleton<IFilterPipeline, FilterPipeline>();
-        return services;
-    }
-
-    public static IServiceCollection AddMinLengthFilter(this IServiceCollection services, int length)
-    {
-        return services.AddSingleton<IWordFilter, MinLengthFilter>(provider => new MinLengthFilter(length));
-    }
-
-    public static IServiceCollection AddCharacterFilter(this IServiceCollection services, char character)
-    {
-        return services.AddSingleton<IWordFilter, ContainsCharacterFilter>(provider => new ContainsCharacterFilter(character));
-    }
-
-    public static IServiceCollection AddMiddleVowelFilter(this IServiceCollection services)
-    {
-        return services.AddSingleton<IWordFilter, MiddleVowelFilter>();
-    }
-}
 
 public interface IFilterPipeline
 {
