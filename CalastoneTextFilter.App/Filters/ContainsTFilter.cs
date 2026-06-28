bb05ac60078ba922c8fc34621926f620c7724f96
Richard Hart <richard.hart@finova.tech>
2026-06-28 08:46:11 +0100

    Added Contains T tests and first pass implementation

---
+++ CalastoneTextFilter.App/Filters/ContainsTFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalastoneTextFilter.App.Filters
+{
+    public class ContainsTFilter : IWordFilter
+    {
+        public bool ShouldKeep(ReadOnlySpan<char> word)
+        {
+            return !MemoryExtensions.Contains(word, "t", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ CalastoneTextFilter.Tests/ContainsTFilterTests.cs
@@ -0,0 +1,36 @@
+﻿using CalastoneTextFilter.App.Filters;
+
+namespace CalastoneTextFilter.Tests
+{
+    public class ContainsTFilterTests
+    {
+        private readonly ContainsTFilter _filter = new();
+
+        [Theory]
+        [InlineData("the")]
+        [InlineData("feet")]
+        [InlineData("fortunately")]
+        [InlineData("to")]
+        [InlineData("Testing")]
+        public void ShouldKeep_ReturnsFalse_WhenWordContainsLowercaseOrUppercaseT(string word)
+        {
+            Assert.False(_filter.ShouldKeep(word));
+        }
+
+        [Theory]
+        [InlineData("Alice")]
+        [InlineData("beginning")]
+        [InlineData("her")]
+        [InlineData("bank")]
+        public void ShouldKeep_ReturnsTrue_WhenWordDoesNotContainT(string word)
+        {
+            Assert.True(_filter.ShouldKeep(word));
+        }
+
+        [Fact]
+        public void ShouldKeep_ReturnsTrue_ForEmptyString()
+        {
+            Assert.True(_filter.ShouldKeep(string.Empty));
+        }
+    }
+}
