f5f802ddc8c532d27e469cf0ba0bf3d474f79762
Richard Hart <richard.hart@finova.tech>
2026-06-29 10:44:26 +0100

    Alternate character test names were confusing. Stick to the same number of tests, but add char to test into the inline data

--- CalastoneTextFilter.Tests/ContainsCharacterFilterTests.cs
+++ CalastoneTextFilter.Tests/ContainsCharacterFilterTests.cs
@@ -4,51 +4,48 @@ namespace CalastoneTextFilter.Tests;
 
 public class ContainsCharacterFilterTests
 {
-    private readonly ContainsCharacterFilter _filterStandardChar = new('t');
-    private readonly ContainsCharacterFilter _filterAlternateChar = new('f');
-
     [Theory]
-    [InlineData("the")]
-    [InlineData("feet")]
-    [InlineData("fortunately")]
-    [InlineData("to")]
-    [InlineData("Testing")]
-    public void ShouldKeep_ReturnsFalse_WhenWordContainsLowercaseOrUppercaseT(string word)
+    //Test 't' char
+    [InlineData('t', "the")]
+    [InlineData('t', "feet")]
+    [InlineData('t', "fortunately")]
+    [InlineData('t', "to")]
+    [InlineData('t', "Testing")]
+
+    //Test 'f' char
+    [InlineData('f', "fool")]
+    [InlineData('f', "flume")]
+    [InlineData('f', "Farmer")]
+    public void ShouldKeep_ReturnsFalse_WhenWordContainsLowercaseOrUppercaseFilterChar(char filterChar, string word)
     {
-        Assert.False(_filterStandardChar.ShouldKeep(word));
+        var charFilter = new ContainsCharacterFilter(filterChar);
+        Assert.False(charFilter.ShouldKeep(word));
     }
 
     [Theory]
-    [InlineData("Alice")]
-    [InlineData("beginning")]
-    [InlineData("her")]
-    [InlineData("bank")]
-    public void ShouldKeep_ReturnsTrue_WhenWordDoesNotContainT(string word)
+    //Test 't' char
+    [InlineData('t', "Alice")]
+    [InlineData('t', "Beginning")]
+    [InlineData('t', "her")]
+    [InlineData('t', "bank")]
+    [InlineData('t', "fool")]
+
+    //Test 'f' char
+    [InlineData('f', "Testing")]
+    [InlineData('f', "the")]
+    [InlineData('f', "Alice")]
+    [InlineData('f', "her")]
+    public void ShouldKeep_ReturnsTrue_WhenWordDoesNotContainFilterChar(char filterChar, string word)
     {
-        Assert.True(_filterStandardChar.ShouldKeep(word));
+        var charFilter = new ContainsCharacterFilter(filterChar);
+        Assert.True(charFilter.ShouldKeep(word));
     }
 
     [Fact]
     public void ShouldKeep_ReturnsTrue_ForEmptyString()
     {
-        Assert.True(_filterStandardChar.ShouldKeep(string.Empty));
-    }
-
-    [Theory]
-    [InlineData("feet")]
-    [InlineData("fortunately")]
-    public void ShouldKeep_ReturnsFalse_WhenWordContainsLowercaseOrUppercaseF(string word)
-    {
-        Assert.False(_filterAlternateChar.ShouldKeep(word));
-    }
-
-    [Theory]
-    [InlineData("the")]
-    [InlineData("to")]
-    [InlineData("Testing")]
-    public void ShouldKeep_ReturnsTrue_WhenWordDoesNotContainF(string word)
-    {
-        Assert.True(_filterAlternateChar.ShouldKeep(word));
+        var charFilter = new ContainsCharacterFilter('t');
+        Assert.True(charFilter.ShouldKeep(string.Empty));
     }
 }
 
